ec36f49e2fbfb0ffd2d48ec8ed200fe066f0a6b4
lucid.james <lucid.james@tinycloud.nz>
2024-04-07 17:55:50 +1200

    Update to main

---
+++ Screeps/config.js
@@ -0,0 +1,14 @@
+// config.js
+module.exports = {
+    roles: {
+        harvester: {
+            pop: 2
+        },
+        builder: {
+            pop: 3
+        },
+        upgrader: {
+            pop: 1
+        }
+    }
+};
--- Screeps/main.js
+++ Screeps/main.js
@@ -1,8 +1,35 @@
+
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
+const config = require('./config');
+
 
 module.exports.loop = function () {
+
+config.roles.forEach(roleConfig => {
+    const role = roleConfig.role;
+    const creepsOfType = _.filter(Game.creeps, creep => creep.memory.role === role);
+    console.log(`${role}s: ${creepsOfType.length}`);
+
+    if (creepsOfType.length < roleConfig.pop) {
+        const newName = `${role.charAt(0).toUpperCase()}${role.slice(1)}${Game.time}`;
+        console.log(`Spawning new ${role}: ${newName}`);
+        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName, {
+            memory: { role: role }
+        });
+    }
+});
+
+if (Game.spawns['Spawn1'].spawning) {
+    const spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+    Game.spawns['Spawn1'].room.visual.text(
+        `🛠️ ${spawningCreep.memory.role}`,
+        Game.spawns['Spawn1'].pos.x + 1,
+        Game.spawns['Spawn1'].pos.y,
+        { align: 'left', opacity: 0.8 }
+    );
+}
+    
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
@@ -11,9 +38,6 @@ module.exports.loop = function () {
         }
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
         }
     }
 }
